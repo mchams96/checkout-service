567f7cf858a17fc1e8178ead1c03cf20b0193e60
Noura Al <102257192+itsaruon@users.noreply.github.com>
2025-11-07 18:37:03 -0500

    Implememt the persistence layer

---
+++ src/main/java/com/checkoutservice/persistence/InMemoryCartRepository.java
@@ -0,0 +1,32 @@
+package com.checkoutservice.persistence;
+
+import com.checkoutservice.domain.cart.*;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+public class InMemoryCartRepository implements CartRepository {
+    private final Map<String, Cart> cartById = new HashMap<>();
+
+    public String create(String currency){
+        String id = UUID.randomUUID().toString();
+        cartById.put(id, new Cart(id, currency));
+        return id;
+    }
+
+    public Cart get(String cartId) throws NoSuchElementException {
+        Cart foundCart = cartById.get(cartId);
+        if (foundCart == null){
+            throw new NoSuchElementException("Cart not found: " + cartId);
+        }
+        return foundCart;
+    }
+
+    public Cart addItem(String cartId, CartItem item){
+        Cart cart = get(cartId);
+        cart.addItem(item);
+        return cart;
+    }
+}
